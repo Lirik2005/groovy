5124e038e8afaaab8b1f3dc0c185867917c83103
Gusakov Kiril <gusakov24@gmail.com>
2022-09-12 17:03:28 +0300

    gitignore add

--- src/com/scripts/OperatorsRuner.groovy
+++ src/com/scripts/OperatorsRuner.groovy
@@ -3,8 +3,12 @@ package com.scripts
 assert 1 + 2 == 3
 assert 4 - 3 == 1
 assert 3 * 5 == 15
-assert 3 / 2 == 1.5
+assert 3 / 2 == 1.5 //В отличие от JAVA тут мы получаем дробное число. Если мы хотим получить целое то нам надо сделать как в строке ниже
+assert 3.intdiv(2) == 1
 assert 1 + 2 == 3
 assert 10 % 3 == 1
 assert 2**3 == 8
 
+def result =(3/2) as Integer //так работает привидение типов в Groovy
+
+println(result<=29)
